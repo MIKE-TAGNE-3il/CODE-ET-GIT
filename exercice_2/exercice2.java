098913ab3309ee781b287ac89c334cc1abaf2b2e
MIKE-TAGNE <tagnefom@3il.fr>
2024-11-26 18:17:13 +0100

    KISS exercice2

--- exercice_2/exercice2.java
+++ exercice_2/exercice2.java
@@ -19,3 +19,44 @@ public class GestionnaireNotes {
         System.out.println("Moyenne : " + moyenne);
     }
 }
+//Correction:
+public class GestionnaireNotes {
+
+    /**
+     * Affiche note et moyennes
+     */ 
+    public void afficherNotesEtMoyenne(String nomEtudiant, int[] notes) {
+        afficherNote(nomEtudiant,notes);
+        sommeNotes(note);
+        afficherMoyenne(somme);
+    }
+    
+    /**
+     * Affichage des notes
+     */
+    public static void afficherNote(String nomEtudiant,int[] notes){
+        System.out.println("Notes de " + nomEtudiant + ":");
+        for (int note : notes) {
+            System.out.println("- " + note);
+        }
+    }
+
+    /**
+     * Calcul de la somme des notes
+     */
+    public static int sommeNotes(int note){
+        int somme = 0;
+        for (int note : notes) {
+            somme += note;
+        }
+        return somme;
+    }
+
+    /**
+     * Affichage de la moyenne
+     */
+    public static afficherMoyenne(int somme){
+        double moyenne = (double) somme / notes.length;
+        System.out.println("Moyenne : " + moyenne);
+    }
+}
